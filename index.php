2e86276feb8603f0e50d2b579acfd926cd1f6cbc
Felipe Rosa Gomes <redesigninformatica@gmail.com>
2022-03-18 05:15:06 -0300

    Metodos de lista

--- index.php
+++ index.php
@@ -2,16 +2,36 @@
 
 require_once("config.php");
 
-$root = new Usuario();
 
-$root->loadbyId(3);
-
-echo $root;
+//Carrega um usuário
+//$root = new Usuario();
+//oot->loadbyId(3);
+//echo $root;
 
 /*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
+
+// Carrega uma lista de usuários
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+// carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("root","garfao");  
+
+
+echo $usuario;
 
 ?>
